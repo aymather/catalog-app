6673793d0360381f325e0422379322edb2088044
Alec Mather <aymather@gmail.com>
2019-02-19 20:24:16 -0600

    set up login_sessions and added login template view

--- views.py
+++ views.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask import session as login_session
+import random, string
 
 app = Flask(__name__)
+
+
+@app.route('/login')
+def Login():
+	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+	login_session['state'] = state
+	return render_template('login.html', session=login_session)
 
 
 @app.route('/')
@@ -9,4 +18,5 @@ def Home():
 	
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5001)
+	app.secret_key = 'super secret key'
+	app.run(host='0.0.0.0', port=5001,debug=True)
